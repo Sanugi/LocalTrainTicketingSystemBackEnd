e53049f2f4501417c64f7bb842f2a1a8f2a62894
Sanugi <divigalpitiyawdwmsg.21@uom.lk>
2025-10-05 16:11:11 +0530

    minor change

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ connection.once("open", () => {
   console.log("Database connected");
 });
 
-//add endpoint here
+//endpoints
 app.use("/api/users", userController);
 app.use("/api/trains", trainController);
 app.use("/api/schedules", scheduleController);
